1bfa71468816d83459dab4633a1908ab3efdf292
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-04-08 19:00:49 -0300

    Update Program.cs

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,12 +1,21 @@
 ﻿//2- Contagem de Ocorrências: Modifique o algoritmo de busca sequencial para contar quantas vezes um número aparece no array
 
-static int ContaOcorrencias(int[] array, int elemento)
+using System.Diagnostics.Contracts;
+
+static int contar(int[] array, int nprocurado)
 {
-    int contador = 0;
+    int contagem = 0;
+
     for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] == elemento)
-            contador++;
-    }
-    return contador;
+        if (array[i] == nprocurado)
+            contagem++;
+
+    return contagem;
 }
+
+int[] n = new int[] { 2, 5, 7, 4, 2, 5, 7, 8, 7 };
+int nprocurado = 7;
+
+int contagem = contar(n, nprocurado);
+
+Console.WriteLine($"o numero {nprocurado} apareceu {contagem} vezes no array.");
