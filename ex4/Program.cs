dab68985ae3e8defe243e190d9f8a5f8a0400e68
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-04-08 19:02:14 -0300

    Update Program.cs

--- ex4/Program.cs
+++ ex4/Program.cs
@@ -1,18 +1,33 @@
 ﻿//4- Maior e Menor Elemento: Use busca sequencial para encontrar o maior e o menor número de um array
 
-static void MaiorMenor(int[] array, out int maior, out int menor)
-{
-    if (array == null || array.Length == 0)
-        throw new ArgumentException("Array não pode ser vazio.");
+﻿using System.Reflection;
 
-    maior = array[0];
-    menor = array[0];
+int[] n = { 50, 100, 200, 240, 210, 270 };
+
+int maior = emaior(n);
+int menor = emenor(n);
+
+Console.WriteLine($"o maior número é: {maior}");
+Console.WriteLine($"o menor número é: {menor}");
+
+int emaior(int[] array)
+{
+    int maior = array[0];
 
     for (int i = 1; i < array.Length; i++)
-    {
         if (array[i] > maior)
             maior = array[i];
+
+    return maior;
+}
+
+int emenor(int[] array)
+{
+    int menor = array[0];
+
+    for (int i = 1; i < array.Length; i++)
         if (array[i] < menor)
             menor = array[i];
-    }
+
+    return menor;
 }
