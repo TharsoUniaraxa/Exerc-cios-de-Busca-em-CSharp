c1497a69da66ecc5f7de84f62e0e81d78a9aa991
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-04-08 19:40:22 -0300

    Update Program.cs

--- ex9/Program.cs
+++ ex9/Program.cs
@@ -1,16 +1,32 @@
 ﻿//9- Busca com Sentinela: Implemente a busca sequencial com um sentinela (adicionando o elemento buscado no final do array para evitar verificações extras)
 
-static int BuscaComSentinela(int[] array, int elemento)
-{
-    int n = array.Length;
-    int[] novoArray = new int[n + 1];
-    Array.Copy(array, novoArray, n);
-    novoArray[n] = elemento;
+int[] numeros = { 10, 25, 30, 45, 60 };
+int alvo = 45;
 
-    int i = 0;
-    while (novoArray[i] != elemento)
+int posicaoEncontrada = BuscarValor(numeros, alvo);
+
+if (posicaoEncontrada != -1)
+{
+    Console.WriteLine($"Número {alvo} encontrado na posição {posicaoEncontrada}.");
+}
+else
+{
+    Console.WriteLine("Número não encontrado.");
+}
+
+static int BuscarValor(int[] vetorOriginal, int elemento)
+{
+    int tamanho = vetorOriginal.Length;
+    int[] vetorComSentinela = new int[tamanho + 1];
+    
+    vetorComSentinela[tamanho] = elemento;
+    Array.Copy(vetorOriginal, vetorComSentinela, tamanho);
+
+    int indice = 0;
+    while (vetorComSentinela[indice] != elemento)
     {
-        i++;
+        indice++;
     }
-    return i == n ? -1 : i;
+
+    return (indice < tamanho) ? indice : -1;
 }
