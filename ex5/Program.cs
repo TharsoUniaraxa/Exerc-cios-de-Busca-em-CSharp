0b0389d5985889cb1ca753edfbdb2dba36211ef4
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-04-08 19:03:13 -0300

    Update Program.cs

--- ex5/Program.cs
+++ ex5/Program.cs
@@ -1,11 +1,20 @@
 ﻿//5- Elemento Múltiplo de X: Dado um array e um número X, encontre o primeiro número que seja múltiplo de X usando busca sequencial
 
-static int BuscaMultiplo(int[] array, int x)
+int[] n = { 5, 13, 15, 18, 20 };
+int x = 3;
+
+int resultado = buscar(n, x);
+
+if (resultado >= 0)
+    Console.WriteLine($"o primeiro numero múltiplo de {x} achado no array é {resultado}.");
+else
+    Console.WriteLine("nenhum numero achado");
+
+static int buscar(int[] array, int x)
 {
     for (int i = 0; i < array.Length; i++)
-    {
         if (array[i] % x == 0)
             return array[i];
-    }
+
     return -1;
 }
