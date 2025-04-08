8de6eb006a3c41f938c8b0a178930165173599a9
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-04-08 19:47:46 -0300

    Update Program.cs

--- ex18/Program.cs
+++ ex18/Program.cs
@@ -1,26 +1,42 @@
 ﻿//8- Posição do Primeiro Elemento: Dado um array ordenado com números repetidos, modifique a busca binária para encontrar a posição da primeira ocorrência de um número
 
-static int PrimeiroIndice(int[] array, int elemento)
-{
-    int indice = -1;
-    int inicio = 0, fim = array.Length - 1;
+int[] dados = { 1, 2, 4, 4, 4, 5, 6, 7 };
 
-    while (inicio <= fim)
+Console.Write("Digite o número que deseja encontrar: ");
+int alvo = int.Parse(Console.ReadLine());
+
+int indiceInicial = LocalizarPrimeiraOcorrencia(dados, alvo);
+
+if (indiceInicial != -1)
+    Console.WriteLine("Primeira ocorrência na posição: " + indiceInicial);
+else
+    Console.WriteLine("Número não encontrado na lista.");
+
+static int LocalizarPrimeiraOcorrencia(int[] vetor, int valor)
+{
+    int esquerda = 0;
+    int direita = vetor.Length - 1;
+    int posicao = -1;
+
+    while (esquerda <= direita)
     {
-        int meio = (inicio + fim) / 2;
-        if (array[meio] == elemento)
+        int centro = (esquerda + direita) / 2;
+
+        if (vetor[centro] == valor)
         {
-            indice = meio;
-            fim = meio - 1;
+            posicao = centro;
+            direita = centro - 1;
         }
-        else if (array[meio] < elemento)
+        else if (vetor[centro] < valor)
         {
-            inicio = meio + 1;
+            esquerda = centro + 1;
         }
         else
         {
-            fim = meio - 1;
+            direita = centro - 1;
         }
     }
-    return indice;
+
+    return posicao;
 }
+
