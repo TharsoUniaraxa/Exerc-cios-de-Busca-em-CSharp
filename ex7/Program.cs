d1b5fb2b28e979b69554e4fa2dbf94842dbfe336
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-04-08 18:45:32 -0300

    Add files via upload

---
+++ ex7/Program.cs
@@ -0,0 +1,12 @@
+﻿//7- Última Ocorrência: Modifique o algoritmo de busca sequencial para retornar a última ocorrência de um elemento no array
+
+static int BuscaUltimaOcorrencia(int[] array, int elemento)
+{
+    int indice = -1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == elemento)
+            indice = i;
+    }
+    return indice;
+}
