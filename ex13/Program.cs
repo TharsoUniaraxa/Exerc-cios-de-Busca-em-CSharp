3099960a884236f11169708a94cb47e0675dd415
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-04-08 19:43:50 -0300

    Update Program.cs

--- ex13/Program.cs
+++ ex13/Program.cs
@@ -1,25 +1,37 @@
 ﻿//3- Múltiplos Elementos: Modifique a busca binária para encontrar todas as ocorrências de um número no array
 
-static List<int> BuscaTodosOcorrencias(int[] array, int elemento)
+int[] sequencia = { 2, 4, 6, 8, 10, 12, 14 };
+
+Console.Write("Digite o número X: ");
+int numeroAlvo = int.Parse(Console.ReadLine());
+
+int valorEncontrado = EncontrarMaiorMenorQue(sequencia, numeroAlvo);
+
+if (valorEncontrado == -1)
+    Console.WriteLine("Não existe número menor que X na lista.");
+else
+    Console.WriteLine("Maior número menor que X: " + valorEncontrado);
+
+static int EncontrarMaiorMenorQue(int[] vetor, int limite)
 {
-    List<int> indices = new List<int>();
-    int indice = BuscaBinaria(array, elemento);
+    int esquerda = 0;
+    int direita = vetor.Length - 1;
+    int candidato = -1;
 
-    if (indice == -1)
-        return indices;
+    while (esquerda <= direita)
+    {
+        int indiceCentral = (esquerda + direita) / 2;
 
-    int i = indice;
-    while (i >= 0 && array[i] == elemento)
-    {
-        indices.Add(i);
-        i--;
+        if (vetor[indiceCentral] < limite)
+        {
+            candidato = vetor[indiceCentral];
+            esquerda = indiceCentral + 1;
+        }
+        else
+        {
+            direita = indiceCentral - 1;
+        }
     }
-    i = indice + 1;
-    while (i < array.Length && array[i] == elemento)
-    {
-        indices.Add(i);
-        i++;
-    }
-    indices.Sort();
-    return indices;
+
+    return candidato;
 }
