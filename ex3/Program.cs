6a9003a11797c843c4717050d16e088022eb242d
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-04-08 19:01:29 -0300

    Update Program.cs

--- ex3/Program.cs
+++ ex3/Program.cs
@@ -1,11 +1,19 @@
 ﻿//3- Busca em Lista de Strings: Dado um array de nomes, crie uma função que recebe um nome e retorna o índice onde ele está localizado
 
-static int BuscaString(string[] nomes, string nome)
+static int buscar(string[] lista, string nome)
 {
-    for (int i = 0; i < nomes.Length; i++)
-    {
-        if (nomes[i].Equals(nome, StringComparison.CurrentCultureIgnoreCase))
+    for (int i = 0; i < lista.Length; i++)
+        if (lista[i] == nome)
             return i;
-    }
+
     return -1;
 }
+
+string[] n = new string[] { "Gabriel", "João", "Tharso", "Marcus" };
+string nprocurado = "Gabriel";
+int indice = buscar(n, nprocurado);
+
+if (indice >= 0)
+    Console.WriteLine($"nome {nprocurado} achado no índice {indice}.");
+else
+    Console.WriteLine($"nome {nprocurado} não achado na lista.");
