53a7203c47f1a2e7cc74ea22bd5004cf8026e98f
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-04-08 18:55:22 -0300

    Update Program.cs

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,11 +1,23 @@
 ﻿//1- Básico: Implemente um algoritmo que realize uma busca sequencial em um array de inteiros e retorne o índice do elemento encontrado
 
-static int BuscaSequencial(int[] array, int elemento)
+static int buscar(int[] array, int valor)
 {
     for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] == elemento)
+        if (array[i] == valor)
             return i;
-    }
+
     return -1;
+}
+
+int[] n = new int[] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+int valor = 30;
+int busca = buscar(n, valor);
+
+if (busca >= 0)
+{
+    Console.WriteLine($"o valor {valor} achado na busca {busca}.");
+}
+else
+{
+    Console.WriteLine("valor não achado.");
 }
