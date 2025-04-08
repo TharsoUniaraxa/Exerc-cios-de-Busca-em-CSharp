74c3b05509e202049dd9c587921b22bd9c0659c2
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-04-08 18:59:51 -0300

    Update Program.cs

--- ex16/Program.cs
+++ ex16/Program.cs
@@ -1,15 +1,25 @@
 ﻿//6- Inserção Ordenada: Use a busca binária para determinar onde um número deveria ser inserido em um array ordenado para manter a ordem
 
-static int InsercaoOrdenada(int[] array, int elemento)
+static int novaposicao(int[] array, int valor)
 {
-    int inicio = 0, fim = array.Length - 1;
-    while (inicio <= fim)
+    int inicio = 0;
+    int fim = array.Length;
+
+    while (inicio < fim)
     {
         int meio = (inicio + fim) / 2;
-        if (array[meio] < elemento)
+
+        if (array[meio] < valor)
             inicio = meio + 1;
         else
-            fim = meio - 1;
+            fim = meio;
     }
+
     return inicio;
 }
+
+int[] n = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+int novon = 10;
+int posicao = novaposicao(n, novon);
+
+Console.WriteLine($"o número {novon} deve estar na posição {posicao}");
