a84943b3be13e9bfb3903d0baf9f5035886e1af2
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-04-08 18:59:03 -0300

    Update Program.cs

--- ex14/Program.cs
+++ ex14/Program.cs
@@ -1,13 +1,15 @@
 ﻿//4- Maior Número Menor que X: Dado um array ordenado e um número X, encontre o maior número menor que X na lista
 
-static int MaiorMenorQueX(int[] array, int x)
+static int maior(int[] array, int x)
 {
+    int inicio = 0;
+    int fim = array.Length - 1;
     int resultado = -1;
-    int inicio = 0, fim = array.Length - 1;
 
     while (inicio <= fim)
     {
         int meio = (inicio + fim) / 2;
+
         if (array[meio] < x)
         {
             resultado = array[meio];
@@ -18,5 +20,15 @@ static int MaiorMenorQueX(int[] array, int x)
             fim = meio - 1;
         }
     }
+
     return resultado;
 }
+
+int[] n = new int[] { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+int x = 7;
+int resultado = maior(n, x);
+
+if (resultado >= 0)
+    Console.WriteLine($"O maior número menor que {x} é {resultado}");
+else
+    Console.WriteLine($"Não existe número menor que {x}");
