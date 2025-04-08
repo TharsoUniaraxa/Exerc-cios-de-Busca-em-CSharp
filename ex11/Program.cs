508697c946a2ddecffc5459c8d18a9512d00829e
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-04-08 19:42:44 -0300

    Update Program.cs

--- ex11/Program.cs
+++ ex11/Program.cs
@@ -1,18 +1,32 @@
 ﻿//1- Básico: Implemente a busca binária em um array ordenado de inteiros e retorne o índice do número procurado
 
-static int BuscaBinaria(int[] array, int elemento)
-{
-    int inicio = 0, fim = array.Length - 1;
+int[] dados = { 2, 4, 6, 8, 10, 12, 14, 16, 18 };
+int alvo;
 
-    while (inicio <= fim)
+Console.Write("Escreva o número que deseja procurar: ");
+alvo = int.Parse(Console.ReadLine());
+
+if (VerificarExistencia(dados, alvo))
+    Console.WriteLine("O número existe na lista.");
+else
+    Console.WriteLine("O número não existe.");
+
+static bool VerificarExistencia(int[] vetorOrdenado, int elemento)
+{
+    int limiteInferior = 0;
+    int limiteSuperior = vetorOrdenado.Length - 1;
+
+    while (limiteInferior <= limiteSuperior)
     {
-        int meio = (inicio + fim) / 2;
-        if (array[meio] == elemento)
-            return meio;
-        if (array[meio] < elemento)
-            inicio = meio + 1;
+        int pontoMedio = (limiteInferior + limiteSuperior) / 2;
+
+        if (vetorOrdenado[pontoMedio] == elemento)
+            return true;
+        else if (vetorOrdenado[pontoMedio] < elemento)
+            limiteInferior = pontoMedio + 1;
         else
-            fim = meio - 1;
+            limiteSuperior = pontoMedio - 1;
     }
-    return -1;
+
+    return false;
 }
