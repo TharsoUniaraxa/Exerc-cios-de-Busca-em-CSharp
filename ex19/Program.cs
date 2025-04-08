d40c70ddf432cd746b0a75e53585cd1d3c46c00f
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-04-08 19:47:08 -0300

    Update Program.cs

--- ex19/Program.cs
+++ ex19/Program.cs
@@ -1,26 +1,42 @@
 ﻿//9- Posição da Última Ocorrência: Dado um array ordenado com números repetidos, modifique a busca binária para encontrar a posição da última ocorrência de um número
 
-static int UltimoIndice(int[] array, int elemento)
-{
-    int indice = -1;
-    int inicio = 0, fim = array.Length - 1;
+int[] dados = { 1, 2, 4, 4, 4, 5, 6, 7 };
 
-    while (inicio <= fim)
+Console.Write("Digite o número que deseja encontrar: ");
+int alvo = int.Parse(Console.ReadLine());
+
+int indiceFinal = BuscarUltimaOcorrencia(dados, alvo);
+
+if (indiceFinal != -1)
+    Console.WriteLine("Última ocorrência na posição: " + indiceFinal);
+else
+    Console.WriteLine("Número não encontrado na lista.");
+
+static int BuscarUltimaOcorrencia(int[] vetor, int valor)
+{
+    int esquerda = 0;
+    int direita = vetor.Length - 1;
+    int posicaoEncontrada = -1;
+
+    while (esquerda <= direita)
     {
-        int meio = (inicio + fim) / 2;
-        if (array[meio] == elemento)
+        int centro = (esquerda + direita) / 2;
+
+        if (vetor[centro] == valor)
         {
-            indice = meio;
-            inicio = meio + 1;
+            posicaoEncontrada = centro;
+            esquerda = centro + 1;
         }
-        else if (array[meio] < elemento)
+        else if (vetor[centro] < valor)
         {
-            inicio = meio + 1;
+            esquerda = centro + 1;
         }
         else
         {
-            fim = meio - 1;
+            direita = centro - 1;
         }
     }
-    return indice;
+
+    return posicaoEncontrada;
 }
+
