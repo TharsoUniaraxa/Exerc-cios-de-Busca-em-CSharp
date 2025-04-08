f2d3c9db0fc4d7ba10975595d725a1e2040f01db
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-04-08 19:39:29 -0300

    Update Program.cs

--- ex8/Program.cs
+++ ex8/Program.cs
@@ -1,11 +1,25 @@
 ﻿//8- Par ou Ímpar: Modifique a busca sequencial para encontrar o primeiro número par no array
 
-static int BuscaPrimeiroPar(int[] array)
+int[] numeros = { 11, 8, 16, 1, 55, 17, 8, 20, 13 };
+int indicePar = LocalizarPrimeiroPar(numeros);
+
+if (indicePar != -1)
 {
-    for (int i = 0; i < array.Length; i++)
+    Console.WriteLine($"O primeiro par é {numeros[indicePar]}");
+}
+else
+{
+    Console.WriteLine("Não há números pares no array");
+}
+
+static int LocalizarPrimeiroPar(int[] lista)
+{
+    for (int contador = 0; contador < lista.Length; contador++)
     {
-        if (array[i] % 2 == 0)
-            return array[i];
+        if (lista[contador] % 2 == 0)
+        {
+            return contador;
+        }
     }
     return -1;
 }
