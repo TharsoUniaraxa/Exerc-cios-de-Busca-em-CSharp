f2e9b47f8e9ec00343979ee8c827aa5728de2253
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-04-08 18:58:07 -0300

    Update Program.cs

--- ex12/Program.cs
+++ ex12/Program.cs
@@ -1,6 +1,31 @@
 ﻿//2- Verificação de Existência: Dado um array ordenado, crie um programa que informe se um número existe ou não na lista, sem precisar retornar o índice
 
-static bool VerificaExistencia(int[] array, int elemento)
+static bool temlista(int[] array, int nprocurado)
 {
-    return BuscaBinaria(array, elemento) != -1;
+    int inicio = 0;
+    int fim = array.Length - 1;
+
+    while (inicio <= fim)
+    {
+        int meio = (inicio + fim) / 2;
+
+        if (array[meio] == nprocurado)
+            return true;
+        else if (array[meio] < nprocurado)
+            inicio = meio + 1;
+        else
+            fim = meio - 1;
+    }
+
+    return false;
 }
+
+int[] n = new int[] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+int nprocurado = 11;
+
+bool tem = temlista(n, nprocurado);
+
+if (tem)
+    Console.WriteLine($"O número {nprocurado} está na lista.");
+else
+    Console.WriteLine($"O número {nprocurado} não está na lista.");
